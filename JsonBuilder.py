eeb055958eebc8e7c02b41669a065d33dfe27497
Jacky Chen <jchen17@magneto.socs.uoguelph.ca>
2016-10-13 11:20:09 -0400

    Fixed JSON Builder

--- JsonBuilder.py
+++ JsonBuilder.py
@@ -5,6 +5,8 @@ import json
 
 ##### The class definitions that support this script.
 
+listOfButtons = []
+
 # The Application class that is used to encapsulate the GUI object.
 class App:
 	# Creates an Application instance.
@@ -37,32 +39,57 @@ class App:
 		py = (self.root.winfo_screenheight() - self.wm_height) / 2
 		self.root.geometry('%dx%d+%d+%d' % (self.wm_width, self.wm_height, px, py))
 		# Main event-based loop for the window.
-		self.root.mainloop()
+		self.root.mainloop();
 
 	# Destroys the window instance.
 	def destroy_Window(self):
 		self.root.destroy()
 
 	# Adds a custom text-field to the Application GUI.
-	def add_TextField(self, tag_name, max_char):
-		# Adding Label to the left-side of the grid.
-		new_label = Label(self.root, text=tag_name)
-		new_label.grid(row=self.wm_num, column=0, padx=12, pady=0, sticky='nsew')
-		# Adding Entry to the right-side of the grid.
-		new_entry = Entry(self.root)
-		new_entry.grid(row=self.wm_num, column=1, padx=12, pady=1, sticky='nsew')
-		# Re-adjust the height of the window.
-		#self.wm_height += 21
-		# Tracking the number of widgets.
-		self.wm_num += 1
+	def add_Fields(self):
+		#Variables
+		listOfFields = ["Name", "Breed", "Gender", "Hi"]
+		listOfDummy = ["Manga", "Norwegian Forest Cat", "Male"]
+		count = 1
 
-	def add_radiobutton(self):
-		R1 = Radiobutton(self.root, text = "Male")
-		R1.pack()
-            
+		def submit_Fields():
+			for x in listOfButtons:
+				inputText = x.get()
+				print inputText
 
+		def auto_Fill():
+			for x in listOfButtons:
+				inputText = x.insert(0, "dummy")
+				#print listOfDummy[i]
 
+		for x in listOfFields:
+			# Adding Label to the left-side of the grid.
+			name = Label(self.root, text=x)
+			name.grid(row=count, column=0, padx=12, pady=0, sticky='nsew')
+			# Adding Entry to the right-side of the grid.
+			nameEntry = Entry(self.root)
+			listOfButtons.append(nameEntry)
+			nameEntry.grid(row=count, column=1, padx=12, pady=1, sticky='nsew')
+			# Re-adjust the height of the window.
+			count = count + 1
 
+		# Submit Button
+		b = Button(self.root, text="Submit", command=submit_Fields)
+		b.grid(row=count, column=1, padx=12, pady=0, sticky='nsew')
+
+		# Autofill Button with dummy input
+		b2 = Button(self.root, text="Autofill", command=auto_Fill)
+		b2.grid(row=count, column=0, padx=12, pady=0, sticky='nsew')
+
+#
+#	def sendJSON():
+#		data = {
+#			'name' : 'ACME',
+#			'breed' : ,
+#			'gender' : 542.23
+#		}
+
+		
 
 
 
@@ -76,8 +103,6 @@ jsonGene = App("JSON Generator")
 # Creating the Application interface.
 jsonGene.create_Window()
 
-jsonGene.add_TextField("Cat's Name", 20)
-jsonGene.add_TextField("Cat's Breed", 20)
-jsonGene.add_TextField("Cat's Gender", 20)
-jsonGene.add_radiobutton()
+jsonGene.add_Fields()
+
 jsonGene.show_Window()
