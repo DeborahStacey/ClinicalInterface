78a5a4f1def2171c7a9a27d70feb9c44fb99b896
James Maher <jmaher@uoguelph.ca>
2016-10-21 20:59:50 -0400

    Finished JSON generation. Auto-fill still needs to be started.

--- Middleware/JsonApp.py
+++ Middleware/JsonApp.py
@@ -1,6 +1,7 @@
 #!c:/Python34/python
 from tkinter import *
 from tkinter import ttk
+from collections import *
 import json
 
 # Generates JSON-file from the list of enteries.
@@ -8,7 +9,7 @@ import json
 #			 The list of data to be included in the JSON-file.
 def write_JsonFile(fileName, fileData):
 	with open(fileName + ".json", "w") as outFile:
-		json.dump(fileData, outFile)
+		json.dump(fileData, outFile, indent=4, separators=(',', ':'))
 	return None
 
 # Reads the list of entries from the JSON-file.
@@ -21,153 +22,180 @@ def read_JsonFile(fileName):
 
 # The Application class that is used to encapsulate the GUI object.
 class JsonApp:
-	# The template of entries for the JSON-file.
-	#		<household>: {
-	#			<entry_01>: { <data> },
-	#			<entry_02>: { <data> },
-	#			<entry_03>: { <data> },
-	#				and so on...
-	tempJSON = {}
-
 	# Creates an Application instance.
 	#	@param:  The name of the Application. 
 	def __init__(self, title):
-		self.wm_title = title
-		self.client_widgets = []
-		self.patient_widgets = []
-		self.patient_count = 1
+		self.windowTitle = title
+		self.setOfWidgets = []
+		self.clientWidgetCount = 0
+		self.patientWidgetCount = 0
 
-# The Command Section of the JsonApp.
+	# Creates prompts for the Application that the user can use to enter information.
+	#	@param:  The frame object that the prompts will be placed into. 
+	#			 The list of prompts with their repsecitve configurations.
 	def newPrompts(self, frame, prompts):
-		# Divide the frame into six equally-spaced columns for prompts.
+		# Divide the frame into twelve equally-spaced columns.
 		for x in range(0, 12):
 			frame.grid_columnconfigure(x, weight=1)
-		# Adding each prompt to their specified parameters.
+		# Constructing each prompt with its own configuration and adding it to the frame.
 		for i in prompts:
-			#
-			configIndex = 2
-			extIndex = 8
-			#
-			numOfWidgets = len(self.patient_widgets)
-			# If prompt is actually a divider, create a horizontal line.
+			# The number of widgets in the list within the current iteration.
+			numOfWidgets = len(self.setOfWidgets)
+			# Indices to each prompt's settings (configuration and extensions).
+			inConfig = 2
+			inExt = 8
+			# Creates a horizontal line-break when the prompt is actally a "divider".
 			if (i[0] == "--divider--"):
 				tempLine = ttk.Separator(frame, orient="horizontal")
 				tempLine.grid(row=i[1], column=i[2], columnspan=12, padx=3, pady=(6,0), sticky='nsew')
 				continue
-			# Otherwise, add the specified prompt to the frame.
 			if (i[0] != "--none--"):
 				if (i[7]):
-					tempLabel = Label(frame, text=str(i[0]+"*:"))
+					newLabel = Label(frame, text=str(i[0]+"*:"))
 				else:
-					tempLabel = Label(frame, text=str(i[0]+":"))
-				tempLabel.grid(row=i[2], column=i[3], padx=0, pady=(6,0), sticky='nsew')
+					newLabel = Label(frame, text=str(i[0]+":"))
+				newLabel.grid(row=i[2], column=i[3], padx=0, pady=(6,0), sticky='nsew')
+			# If the prompt has no name, it's assumed as another widget for the previous one.
 			else:
-				configIndex = 1
-				extIndex = 6
-			#	
-			if (i[configIndex+4] == "text"):
-				tempWidget = Entry(frame)
-				tempWidget.grid(row=i[configIndex], column=i[configIndex+1]+1, rowspan=i[configIndex+2], 
-									columnspan=i[configIndex+3], padx=(0,6), pady=(6,0), sticky='nsew')
-			elif (i[configIndex+4] == "menu"):
-				tempWidget = ttk.Combobox(frame, width=10, state="readonly", values=i[extIndex])
-				tempWidget.grid(row=i[configIndex], column=i[configIndex+1]+1, rowspan=i[configIndex+2], 
-									columnspan=i[configIndex+3], padx=(0,6), pady=(6,0), sticky='nsew')
-			elif (i[configIndex+4] == "spinbox-i"):
-				tempWidget = Spinbox(frame, from_=i[extIndex][0], to=i[extIndex][1], increment=i[extIndex][2])
-				tempWidget.grid(row=i[configIndex], column=i[configIndex+1]+1, rowspan=i[configIndex+2], 
-									columnspan=i[configIndex+3], padx=(0,6), pady=(6,0), sticky='nsew')	
-				tempWidget.delete(0, "end")
-				tempWidget.insert(0, 0)
-			elif (i[configIndex+4] == "spinbox-f"):
-				tempWidget = Spinbox(frame, format="%.1f", from_=i[extIndex][0], to=i[extIndex][1], 
-										increment=i[extIndex][2])
-				tempWidget.grid(row=i[configIndex], column=i[configIndex+1]+1, rowspan=i[configIndex+2], 
-									columnspan=i[configIndex+3], padx=(0,6), pady=(6,0), sticky='nsew')
-				tempWidget.delete(0, "end")
-				tempWidget.insert(0, 0)
-			#
+				inConfig = 1
+				inExt = 6
+			# Construction of the specified prompt (labelled and no-name)
+			if (i[inConfig+4] == "text"):
+				newWidget = Entry(frame)
+				newWidget.grid(row=i[inConfig], column=i[inConfig+1]+1, rowspan=i[inConfig+2], 
+									columnspan=i[inConfig+3], padx=(0,6), pady=(6,0), sticky='nsew')
+			elif (i[inConfig+4] == "menu"):
+				newWidget = ttk.Combobox(frame, width=10, state="readonly", values=i[inExt])
+				newWidget.grid(row=i[inConfig], column=i[inConfig+1]+1, rowspan=i[inConfig+2], 
+									columnspan=i[inConfig+3], padx=(0,6), pady=(6,0), sticky='nsew')
+			elif (i[inConfig+4] == "spinbox-i"):
+				newWidget = Spinbox(frame, from_=i[inExt][0], to=i[inExt][1], increment=i[inExt][2])
+				newWidget.grid(row=i[inConfig], column=i[inConfig+1]+1, rowspan=i[inConfig+2], 
+									columnspan=i[inConfig+3], padx=(0,6), pady=(6,0), sticky='nsew')	
+				newWidget.delete(0, "end")
+				newWidget.insert(0, 0)
+			elif (i[inConfig+4] == "spinbox-f"):
+				newWidget = Spinbox(frame, format="%.1f", from_=i[inExt][0], to=i[inExt][1], 
+										increment=i[inExt][2])
+				newWidget.grid(row=i[inConfig], column=i[inConfig+1]+1, rowspan=i[inConfig+2], 
+									columnspan=i[inConfig+3], padx=(0,6), pady=(6,0), sticky='nsew')
+				newWidget.delete(0, "end")
+				newWidget.insert(0, 0)
+			# The widgets associated with the prompts are stored for future JSON-assembly.
+			#	- Prompts with no name, have their widgets included in the previous prompt.
 			if (i[0] != "--none--"):
-				self.patient_widgets.append({"tag-name": i[1],"widget": [tempWidget], "is-required": i[7]})
+				self.setOfWidgets.append({"tag-name": i[1],"widget": [newWidget], "is-required": i[7]})
 			else:
-				self.patient_widgets[numOfWidgets-1]["widget"].append(tempWidget)
+				self.setOfWidgets[numOfWidgets-1]["widget"].append(newWidget)
 
-	def newAccountPrompts(self, accountPrompts):
-		# Temp-frame to be added to the GUI.
+	# Creates prompts for the account-section of the Application.
+	#	@param:  The list of account-related prompts with their repsecitve configurations.
+	def newAccount(self, accountPrompts):
 		tempFrame = Frame(self.root)
 		self.newPrompts(tempFrame, accountPrompts)
-		# Place the Account prompts on the top-section of the Application.
 		tempFrame.grid(row=0, column=0, columnspan=4, sticky='nsew')
 
-	def newPatientPrompts(self, patientPrompts):
-		# Temp-frame to be added to the GUI.
+	# Creates prompts for each patient-tab in the Notebook.
+	#	@param:  The list of patient-related prompts with their repsecitve configurations.
+	def newPatient(self, patientPrompts):
 		tempFrame = Frame(self.root)
 		self.newPrompts(tempFrame, patientPrompts)
-		# Place the Patient prompts in a new tab of the Notebook.
-		self.notebook.add(tempFrame, text=("Pet-"+str(self.patient_count)))
-		self.patient_count += 1
+		self.notebook.add(tempFrame, text=("Pet-"+str(len(self.notebook.tabs()))+""))
 
-		print ("We are adding a tab")
+	# Removes the selected patient-tab from the Notebook (unless it's the only one).
+	def removePatient(self):
+		if (len(self.notebook.tabs()) > 1):
+			self.notebook.forget(self.notebook.select())
 
-	def remove_Patient(self):
-		print ("We are removing a tab")
+	# Randomly enters information into the prompts for all sections.
+	def autofill(self):
+		print ("Auto-fill")
 		return
 
-	def submit_JsonApp(self):
-		print ("We are submitting it all")
-		for i in self.client_widgets:
-			for j in i["widget"]:
-				print (j.get())
-		for i in self.patient_widgets:
-			for j in i["widget"]:
-				print (j.get())
-		return
+	# Compiles an ordered-set of values from the set of widgets into a JSON-object.
+	def submitProfile(self):
+		# The master-list of values that is used to construct the JSON-object.
+		setOfValues = OrderedDict()
+		# Traversing the master-list of widgets and sending inputs to an ordered-set.
+		for i in range(0, len(self.setOfWidgets)):
+			# The index of the patient that is currently being added to the set.
+			inPatient = int((i-self.clientWidgetCount) / self.patientWidgetCount)
+			print (inPatient)
+			# The parameters of the widget sets that are used to rettrieve information.
+			paramName = self.setOfWidgets[i]["tag-name"]
+			paramWidget = self.setOfWidgets[i]["widget"]
+			paramState = self.setOfWidgets[i]["is-required"]
+			# The account widgets are always handled before all other sections.
+			if (i < self.clientWidgetCount):
+				setOfValues.update({paramName: paramWidget[0].get()})
+				for n in range(1, len(paramWidget)):
+					setOfValues.update({paramName: " "+paramWidget[n].get()})
+			# Separate sets are created for each tab (represents each patient).
+			elif ((i-self.clientWidgetCount) % self.patientWidgetCount == 0):
+				# Creates a new set for the next patient and their respective values.
+				#	- If the account widgets were recently completed, create "Household" group.
+				if (i == self.clientWidgetCount):
+					setOfValues.update({"houseHold": [OrderedDict()]})
+				else:
+					setOfValues["houseHold"].append(OrderedDict())
+				setOfValues["houseHold"][inPatient].update({paramName: paramWidget[0].get()})
+				for n in range(1, len(paramWidget)):
+					setOfValues["houseHold"][inPatient].update({paramName: " "+paramWidget[n].get()})
+			# The patient widgets are grouped for each tab in the Notebook. 
+			else:
+				setOfValues["houseHold"][inPatient].update({paramName: str(paramWidget[0].get())})
+				for n in range(1, len(paramWidget)):
+					setOfValues["houseHold"][inPatient].update({paramName: " "+str(paramWidget[n].get())})
+		# Create the JSON-File and exit the program.
+		write_JsonFile("temp", setOfValues)
+		self.root.destroy()
 
-	def cancel_JsonApp(self):
-		print ("We are cancelling it all")
-		return
-
-# The Interface Section of the JsonApp.
 	# Creates a non-resizable window instance (other variables are set separately).
 	#	@param:	 The list of prompts to be included in the App.
 	def create_Window(self, accountPrompts, patientPrompts):
 		# Main root instance for the window.
 		self.root = Tk()
-		self.root.wm_title(self.wm_title);
+		self.root.wm_title(self.windowTitle);
 		self.root.resizable(width=False, height=False)
 		# Organize the window into sections for easier layout management.
 		for x in range(0, 4):
 			self.root.grid_columnconfigure(x, weight=1)
+		# Determines the number of valid prompts in each list.
+		for i in accountPrompts:
+			if (i[0] == "--divider--" or i[0] == "--none--"):
+				continue
+			self.clientWidgetCount += 1
+		for i in patientPrompts:
+			if (i[0] == "--divider--" or i[0] == "--none--"):
+				continue
+			self.patientWidgetCount += 1
 		
-		# The HouseHold Prompt that is used to name the Json-file.
-		self.newAccountPrompts(accountPrompts)
-
+		# Adding a starting patient-tab for the Application.
+		self.newAccount(accountPrompts)
 		# Horizontal line that separates this section and the next.
 		tempLine1 = ttk.Separator(self.root, orient="horizontal")
 		tempLine1.grid(row=1, columnspan=4, padx=0, pady=(6,0), sticky="nsew")
-
 		# Creating a Notebook(Tabs) for the patients.
 		self.notebook = ttk.Notebook(self.root)
 		self.notebook.grid(row=2, column=0, columnspan=4, padx=(5,2), pady=6, sticky='nsew')
-		# Adding a starting client-tab for the Notebook.
-		self.newPatientPrompts(patientPrompts)
-
+		# Adding a starting patient-tab for the Notebook.
+		self.newPatient(patientPrompts)
 		# Horizontal line that separates this section and the next.
 		tempLine2 = ttk.Separator(self.root, orient="horizontal")
 		tempLine2.grid(row=3, columnspan=4, padx=0, pady=(0,12), sticky="nsew")
 
-		# Add Button
-		btnInsert = Button(self.root, text="Add", command=lambda: self.newPatientPrompts(patientPrompts))
-		btnInsert.grid(row=4, column=0, padx=12, pady=(0,6), sticky='nsew')
-		# Remove Button
-		btnRemove = Button(self.root, text="Remove", command=self.remove_Patient)
-		btnRemove.grid(row=4, column=1, padx=12, pady=(0,6), sticky='nsew')
-		# Submit Button
-		btnSubmit = Button(self.root, text="Submit", command=self.submit_JsonApp)
-		btnSubmit.grid(row=4, column=2, padx=12, pady=(0,6), sticky='nsew')
-		# Cancel Button
-		btnCancel = Button(self.root, text="Cancel", command=self.cancel_JsonApp)
-		btnCancel.grid(row=4, column=3, padx=12, pady=(0,6), sticky='nsew')
+		# Creates the "New Patient" button that inserts a new tab into the Notebook.
+		addButton = Button(self.root, text="New Patient", command=lambda: self.newPatient(patientPrompts))
+		addButton.grid(row=4, column=0, padx=12, pady=(0,6), sticky='nsew')
+		# Creates the "Remove" button that removes the selected tab from the Notebook.
+		removeButton = Button(self.root, text="Remove", command=self.removePatient)
+		removeButton.grid(row=4, column=1, padx=12, pady=(0,6), sticky='nsew')
+		# Creates the "Auto-Fill" button that generates values for all the prompts.
+		autoFillButton = Button(self.root, text="Auto-Fill", command=self.autofill)
+		autoFillButton.grid(row=4, column=2, padx=12, pady=(0,6), sticky='nsew')
+		# Creates the "Register" button that generates the JSON-object and exits the program.
+		registerButton = Button(self.root, text="Register", command=self.submitProfile)
+		registerButton.grid(row=4, column=3, padx=12, pady=(0,6), sticky='nsew')
 
 	# Presents the window instance to the user.
 	def show_Window(self):
