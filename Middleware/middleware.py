120f1b74535f55ec58f840fb7a0c416455ac99f9
Taha <taha-ansari@hotmail.com>
2016-10-27 11:24:58 -0400

    example NEW json object

--- Middleware/middleware.py
+++ Middleware/middleware.py
@@ -1,3 +1,16 @@
-import sys
+import json
 
 
+def readJson(objName):	
+	objFile = open(objName, 'r')
+	jsonString = objFile.read()	
+	parsedObj = json.loads(jsonString)
+	#print (parsedObj)
+	recieveJson(parsedObj)
+
+def recieveJson(parsedObj):
+	print (parsedObj[0]["ownerid"])
+
+
+
+readJson("tansari.json")	
