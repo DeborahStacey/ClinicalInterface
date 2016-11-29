e8233508fb0c23f0a965d09eb4f4a1a1727181d6
Sameer <sgulamal@mail.uoguelph.ca>
2016-11-28 23:21:24 -0500

    added inputs

---
+++ Interface/InputVariables.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+# A generalized compilation of all available animal breeds.
+constSpecies = [
+    "Feline", "Canine"]
+constBreeds = {
+    "Feline":
+        ["American Short-Hair", "British Short-Hair", "Cymric",
+            "Himalayan", "Japanese Bobtail", "Munchkin", "Persian",
+            "Somali", "Turkish Van"],
+    "Canine":
+        ["Beagle", "German Shepard", "Rottweiler", "Shih Tzu",
+            "Siberian Husky", "Dachshund", "Golden Retriever",
+            "Shiba Inu", "Border Collie"]}
+
+# A generalized compilation of all available sexes.
+constGenders = [
+    "Male", "Male (Neutered)", "Female", "Female (Spayed)"]
+
+# A generalized compilation of all custom lifestyles.
+constLifeStyles = [
+    "HousePet (Indoor)", "HousePet (Outdoor)"]
+
+# A generalized compilation of all available names.
+constNames = [
+	"Bella", "Tigger", "Chloe", "Shadow", "Luna", "Oreo",
+	"Oliver", "Kitty", "Lucy", "Molly", "Jasper", "Smokey", 
+	"Gizmo", "Simba", "Tiger", "Charlie", "Angel", "Jack",
+	"Lily", "Peanut", "Toby", "Baby", "Loki", "Midnight", "Milo",
+	"Sophie", "Harley", "Max", "Missy", "Rocky", "Zoe","CoCo", 
+	"Misty", "Pepper", "Oscar", "Sassy", "Bandit", "Boots", 
+	"Felix", "Tucker", "Simon", "Lucky", "Kiki", "Buddy"]
+
+# A generalized compilation of all available causes of death.
+constDeathReasons = [
+    "Cardiac Disease.", "Parasitic Disorder.", "Neoplasia.",
+    "Life-Threatening Injuries.", "Anaemia.", "Malnutrition.", 
+    "Dehydration.","Cancer-related Death.", 
+    "No Specific Cause of Death."]
