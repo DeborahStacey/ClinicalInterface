4c211c9ae636885a6a85febf1cbafbd546acdb7b
Sameer <sgulamal@mail.uoguelph.ca>
2016-11-28 23:20:42 -0500

    changed text area

--- Interface/ClinicalGui.py
+++ Interface/ClinicalGui.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 from collections import *
 import json
 from jsonTest import *
+from InputVariables import *
 
 # Generates JSON-file from the list of enteries.
 #	@param:  The name of the JSON-file to be generated.
@@ -31,16 +32,16 @@ class ClinicalGui:
 		self.clientWidgetCount = 0
 		self.patientWidgetCount = 0
 
-	
 
-	# Randomly enters information into the prompts for all sections. (here just incase)
-	def autofill(self):
-		print ("Auto-fill")
-		return
+	def updateBreedWidget(self, widgetSpecies, widgetBreeds):
+		if (widgetSpecies.get() == ""):
+			widgetBreeds.config(values=[])
+		else:
+			widgetBreeds.config(values=constBreeds[widgetSpecies.get()])
 
 	# This function takes all the values and constructs a JSON from all the fields 
 	def Search(self):
-		# this function does nothing as of yet
+		
 		testVar = []
 		requests = []
 		if(widgetOwnerID.get()):
@@ -99,123 +100,136 @@ class ClinicalGui:
 		self.root.resizable(width=True, height=True)
 
 
+
 		# Widgets for the OwnerID and PetID fields (disabled state).
-		Label(self.root, text="Owner ID:").grid(row=0, column=0, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Owner ID:").grid(row=0, column=0, padx=0, pady=(15,0), sticky='nsew')
 		global widgetOwnerID
 		widgetOwnerID = Entry(self.root)
-		widgetOwnerID.grid(row=0, column=1, columnspan=5, padx=(0,6), pady=(6,0), sticky='nsew')
+		widgetOwnerID.grid(row=0, column=1, columnspan=5, padx=(0,6), pady=(15,0), sticky='nsew')
 		self.setOfWidgets.append(widgetOwnerID)
 
-		Label(self.root, text="Pet ID:").grid(row=0, column=6, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Pet ID:").grid(row=0, column=6, padx=0, pady=(15,0), sticky='nsew')
 		global widgetPetID
 		widgetPetID = Entry(self.root)
-		widgetPetID.grid(row=0, column=7, columnspan=5, padx=(0,6), pady=(6,0), sticky='nsew')
+		widgetPetID.grid(row=0, column=7, columnspan=5, padx=(0,6), pady=(15,0), sticky='nsew')
 		self.setOfWidgets.append(widgetPetID)
 
 		# Horizontal line that separates the widgets as groups.
-		ttk.Separator(self.root, orient="horizontal").grid(row=1, columnspan=12, padx=3, pady=(6,0), sticky='nsew')
+		ttk.Separator(self.root, orient="horizontal").grid(row=1, columnspan=12, padx=3, pady=(15,0), sticky='nsew')
 
 		# Widgets for the fields regarding general information of the pet (enabled state).
-		Label(self.root, text="Name:").grid(row=2, column=0, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Name:").grid(row=2, column=0, padx=0, pady=(15,0), sticky='nsew')
 		global widgetPetName
 		widgetPetName = Entry(self.root)
-		widgetPetName.grid(row=2, column=1, columnspan=11, padx=(0,6), pady=(6,0), sticky='nsew')
+		widgetPetName.grid(row=2, column=1, columnspan=11, padx=(0,6), pady=(15,0), sticky='nsew')
 		self.setOfWidgets.append(widgetPetName)
 		
-		Label(self.root, text="Breed:").grid(row=3, column=0, padx=0, pady=(6,0), sticky='nsew')
+		#Label(self.root, text="Species:").grid(row=3, column=0, padx=0, pady=(15,0), sticky='nsew')
+		#global widgetSpecies
+		#widgetSpecies = ttk.Combobox(self.root, width=1, state="readonly", values=constSpecies)
+		#widgetSpecies.grid(row=3, column=1, columnspan=5, padx=(0,6), pady=(15,0), sticky='nsew')
+		#widgetSpecies.bind("<<ComboboxSelected>>", lambda e:self.updateBreedWidget(widgetSpecies, widgetBreed))
+		#self.setOfWidgets.append(widgetSpecies)
+
+
+		Label(self.root, text="Breed:").grid(row=3, column=0, padx=0, pady=(15,0), sticky='nsew')
 		global widgetBreed
-		widgetBreed = ttk.Combobox(self.root, width=1, state="readonly", values=["Feline", "Improvius", "Coder", "Persian"])
-		widgetBreed.grid(row=3, column=1, columnspan=5, padx=(0,6), pady=(6,0), sticky='nsew')
+		widgetBreed = ttk.Combobox(self.root, width=1, state="readonly")
+		widgetBreed.grid(row=3, column=1, columnspan=5, padx=(0,6), pady=(15,0), sticky='nsew')
 		self.setOfWidgets.append(widgetBreed)
 		
-		Label(self.root, text="Gender:").grid(row=3, column=6, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Gender:").grid(row=3, column=6, padx=0, pady=(15,0), sticky='nsew')
 		global widgetGender
-		widgetGender = ttk.Combobox(self.root, width=1, state="readonly", values=["male","female","whatever is left","test"])
-		widgetGender.grid(row=3, column=7, columnspan=5, padx=(0,6), pady=(6,0), sticky='nsew')
+		widgetGender = ttk.Combobox(self.root, width=1, state="readonly", values=constGenders)
+		widgetGender.grid(row=3, column=7, columnspan=5, padx=(0,6), pady=(15,0), sticky='nsew')
 		self.setOfWidgets.append(widgetGender)
 
-		Label(self.root, text="Microchip:").grid(row=4, column=0, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Microchip:").grid(row=4, column=0, padx=0, pady=(15,0), sticky='nsew')
 		global widgetMicrochip
 		widgetMicrochip = Checkbutton(self.root)
 		widgetMicrochip.grid(row=4, column=1, columnspan=3, sticky='ws')
 		self.setOfWidgets.append(widgetMicrochip)
 		
-		Label(self.root, text="FitCat:").grid(row=4, column=4, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="FitCat:").grid(row=4, column=4, padx=0, pady=(15,0), sticky='nsew')
 		global widgetFitCat
 		widgetFitCat = Checkbutton(self.root)
 		widgetFitCat.grid(row=4, column=5, columnspan=3, sticky='ws')
 		self.setOfWidgets.append(widgetFitCat)
 		
-		Label(self.root, text="Date of Birth:").grid(row=6, column=0, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Date of Birth:").grid(row=6, column=0, padx=0, pady=(15,0), sticky='nsew')
 		global widgetDateBirth
 		widgetDateBirth = Entry(self.root)
-		widgetDateBirth.grid(row=6, column=1, columnspan=5, padx=(0,6), pady=(6,0), sticky='nsew')
+		widgetDateBirth.grid(row=6, column=1, columnspan=5, padx=(0,6), pady=(15,0), sticky='nsew')
 		self.setOfWidgets.append(widgetDateBirth)
 
-		Label(self.root, text="Date of Death:").grid(row=6, column=6, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Date of Death:").grid(row=6, column=6, padx=0, pady=(15,0), sticky='nsew')
 		global widgetDeathDate
 		widgetDeathDate = Entry(self.root)
-		widgetDeathDate.grid(row=6, column=7, columnspan=5, padx=(0,6), pady=(6,0), sticky='nsew')
+		widgetDeathDate.grid(row=6, column=7, columnspan=5, padx=(0,6), pady=(15,0), sticky='nsew')
 		self.setOfWidgets.append(widgetDeathDate)
 
-		Label(self.root, text="Reason for Death:").grid(row=7, column=6, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Reason for Death:").grid(row=7, column=6, padx=0, pady=(15,0), sticky='nsew')
 		global widgetReasonDeath
 		widgetReasonDeath = Entry(self.root)
-		widgetReasonDeath.grid(row=7, column=8, columnspan=5, padx=(0,6), pady=(6,0), sticky='nsew')
+		widgetReasonDeath.grid(row=7, column=8, columnspan=5, padx=(0,6), pady=(15,0), sticky='nsew')
 		self.setOfWidgets.append(widgetReasonDeath)
 
 		# Horizontal line that separates the widgets as groups.
-		ttk.Separator(self.root, orient="horizontal").grid(row=8, columnspan=12, padx=3, pady=(6,0), sticky='nsew')
+		ttk.Separator(self.root, orient="horizontal").grid(row=8, columnspan=12, padx=3, pady=(15,0), sticky='nsew')
 
 		# Widgets for the fields regarding specific information of the pet (enabled state).
-		Label(self.root, text="Wt. (lbs):").grid(row=9, column=0, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Wt. (lbs):").grid(row=9, column=0, padx=0, pady=(15,0), sticky='nsew')
 		global widgetWeight
 		widgetWeight = Entry(self.root, width=12)
-		widgetWeight.grid(row=9, column=1, columnspan=2, padx=(0,6), pady=(6,0), sticky='w')
+		widgetWeight.grid(row=9, column=1, columnspan=2, padx=(0,6), pady=(15,0), sticky='w')
 		self.setOfWidgets.append(widgetWeight)
 
-		Label(self.root, text="Ht. (cm):").grid(row=9, column=4, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Ht. (cm):").grid(row=9, column=4, padx=0, pady=(15,0), sticky='nsew')
 		global widgetHeight
 		widgetHeight = Entry(self.root, width=12)
-		widgetHeight.grid(row=9, column=5, columnspan=2, padx=(0,6), pady=(6,0), sticky='w')
+		widgetHeight.grid(row=9, column=5, columnspan=2, padx=(0,6), pady=(15,0), sticky='w')
 		self.setOfWidgets.append(widgetHeight)
 
-		Label(self.root, text="Len. (cm):").grid(row=9, column=9, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Len. (cm):").grid(row=9, column=9, padx=0, pady=(15,0), sticky='nsew')
 		global widgetLength
 		widgetLength = Entry(self.root, width=12)
-		widgetLength.grid(row=9, column=10, columnspan=2, padx=(0,6), pady=(6,0), sticky='w')
+		widgetLength.grid(row=9, column=10, columnspan=2, padx=(0,6), pady=(15,0), sticky='w')
 		self.setOfWidgets.append(widgetLength)
 		
 		# Horizontal line that separates the widgets as groups.
-		ttk.Separator(self.root, orient="horizontal").grid(row=10, columnspan=12, padx=3, pady=(6,0), sticky='nsew')
+		ttk.Separator(self.root, orient="horizontal").grid(row=10, columnspan=12, padx=3, pady=(15,0), sticky='nsew')
 
 		# Widgets for the DateUpdated and DateAdded fields.
-		Label(self.root, text="Date Updated:").grid(row=11, column=0, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Date Updated:").grid(row=11, column=0, padx=0, pady=(15,0), sticky='nsew')
 		global widgetDateUpdate
 		widgetDateUpdate = Entry(self.root)
-		widgetDateUpdate.grid(row=11, column=1, columnspan=5, padx=(0,6), pady=(6,0), sticky='nsew')
+		widgetDateUpdate.grid(row=11, column=1, columnspan=5, padx=(0,6), pady=(15,0), sticky='nsew')
 		self.setOfWidgets.append(widgetDateUpdate)
 
-		Label(self.root, text="Date Registered:").grid(row=11, column=6, padx=0, pady=(6,0), sticky='nsew')
+		Label(self.root, text="Date Registered:").grid(row=11, column=6, padx=0, pady=(15,0), sticky='nsew')
 		global widgetDateAdded
 		widgetDateAdded = Entry(self.root)
-		widgetDateAdded.grid(row=11, column=7, columnspan=5, padx=(0,6), pady=(6,0), sticky='nsew')
+		widgetDateAdded.grid(row=11, column=7, columnspan=5, padx=(0,6), pady=(15,0), sticky='nsew')
 		self.setOfWidgets.append(widgetDateAdded)
 
 		# Horizontal line that separates the widgets as groups.
-		ttk.Separator(self.root, orient="horizontal").grid(row=12, columnspan=12)
+		ttk.Separator(self.root, orient="horizontal").grid(row=12, columnspan=12, pady=(15,0))
 
-		
-		
 		# Search button that takes all input and constructs a JSON object to query to the database
 		searchButton = Button(self.root, text="Search", command=self.Search)
-		searchButton.grid(row=13, column=0, columnspan=12, padx=12, pady=(0,6), sticky='nsew')
+		searchButton.grid(row=13, column=0, columnspan=12, padx=12, pady=(15,0), sticky='nsew')
 
-		#labelframe = ttk.LabelFrame(self.root, text="Search Results").grid(row=14,columnspan = 12)
+
+		#global results
+		#results = Text(self.root, height=20, width=20, state = NORMAL)
+		#results.grid( column = 50, columnspan=12, padx=12,pady=(15,6))
+		#results.configure(state='disabled')
+		Label(self.root, text="Search Results:").grid(row=0, column=20, padx=(30,0), pady=(15,0), sticky='nsew')
 		global T
-		T = Text(self.root, height=20, width=70, state = NORMAL)
-		T.grid(row=15, columnspan=12, padx=12,pady=(0,6))
+		T = Text(self.root, height=25, width=70, state = NORMAL)
+		T.grid(row=1, column=15, rowspan = 14, columnspan=14, padx=5,pady=5)
 		T.configure(state='disabled')
+
 	# Presents the window instance to the user.
 	def show_Window(self):
 		self.root.mainloop();
